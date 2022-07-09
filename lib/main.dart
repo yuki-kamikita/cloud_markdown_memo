59bff9862765f750367f4f09c8c0bd736a406f29
yuki_kamikita <akaiyukiusagi@gmail.com>
2022-07-09 22:18:42 +0900

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'ui/Home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+final themeModeProvider =
+    StateProvider((ref) => ThemeMode.light); // FIXME: なんでここ切り替えると一瞬クラッシュするの
+
+/// Themeの定義だけ
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      themeMode: ref.watch(themeModeProvider), // ユーザー設定でここの値を変更できるようにすればいいな
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          useMaterial3: true,
+          colorSchemeSeed: const Color(0xFF6750A4),
+          // colorScheme: lightColorScheme,
+          textTheme: GoogleFonts.yomogiTextTheme(
+            Theme.of(context).textTheme,
+          )),
+      darkTheme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: const Color(0xFF6750A4),
+          brightness: Brightness.dark,
+          // colorScheme: darkColorScheme,
+          textTheme: GoogleFonts.yomogiTextTheme(
+            ThemeData(brightness: Brightness.dark).textTheme,
+          )), // https://itome.team/blog/2019/12/flutter-advent-calendar-day12/
+      home: const Home(),
     );
   }
 }
